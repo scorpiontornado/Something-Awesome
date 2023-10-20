186424d4e47e30f62fcd34d46f2d28aacf69ffff
Nicholas Langford <langfordn@icloud.com>
2023-10-20 15:50:05 +1100

    Working SQL query viewer

--- static/script.js
+++ static/script.js
@@ -1,11 +1,16 @@
-const sqli1_user_in = document.getElementById("sqli1_form_username");
-const sqli1_user_out = document.getElementById("sqli1_query_username");
-sqli1_user_in.addEventListener("input", (e) => {
-  sqli1_user_out.textContent = sqli1_user_in.textContent;
-});
+// const sqli1_user_in = document.getElementById("sqli1_form_username");
+// const sqli1_user_out = document.getElementById("sqli1_query_username");
+// sqli1_user_in.addEventListener("input", (e) => {
+//   sqli1_user_out.textContent = sqli1_user_in.textContent;
+// });
 
-const sqli1_pass_in = document.getElementById("sqli1_form_password");
-const sqli1_pass_out = document.getElementById("sqli1_query_password");
-sqli1_pass_in.addEventListener("input", (e) => {
-  sqli1_pass_out.textContent = sqli1_pass_in.textContent;
-});
+// const sqli1_pass_in = document.getElementById("sqli1_form_password");
+// const sqli1_pass_out = document.getElementById("sqli1_query_password");
+// sqli1_pass_in.addEventListener("input", (e) => {
+//   sqli1_pass_out.textContent = sqli1_pass_in.textContent;
+// });
+
+function mirror(in_id, out_id) {
+  console.log(in_id, out_id);
+  document.getElementById(out_id).textContent = document.getElementById(in_id).value;
+}
