1b020b2e9ff5bc167c0c6f97f607d8a54a821df8
Nicholas Langford <langfordn@icloud.com>
2023-11-08 17:11:57 +1100

    Move sql comments to avoid clutter when performing SQLI

--- vulnerable_webapp_scorpiontornado/sqli2_schema.sql
+++ vulnerable_webapp_scorpiontornado/sqli2_schema.sql
@@ -5,7 +5,11 @@
 -- Inspiration drawn from COMP3311 23T3 assignment 2 (https://webcms3.cse.unsw.edu.au/COMP3311/23T3/resources/89176)
 -- and the COMP6841 SQLI pre-recorded video (https://youtu.be/bAhvzXfuhg8)
 
--- Note - unis really do keep all this info (and more) about you: https://www.student.unsw.edu.au/privacy
+-- Notes:
+--  Unis really do keep all this info (and more) about you: https://www.student.unsw.edu.au/privacy
+--      Could've also included personal email, country of origin, program of study, fee payment info,
+--      financial assistance and scholarships, disciplinary info etc. but that's too much for this CTF.
+--  Also, date_of_birth is an ISO8601 string in the form "YYYY-MM-DD"
 DROP TABLE IF EXISTS Students;
 CREATE TABLE Students(
     student_id INTEGER CHECK (student_id BETWEEN 1000000 AND 9999999),
@@ -14,19 +18,22 @@ CREATE TABLE Students(
     email TEXT,
     phone_number TEXT,
     home_address TEXT,
-    date_of_birth TEXT, -- ISO8601 string in the form "YYYY-MM-DD"
-    -- Could've also included personal email, country of origin, program of study, fee payment info,
-    -- financial assistance and scholarships, disciplinary info etc. but that's too much for this CTF.
+    date_of_birth TEXT,
     PRIMARY KEY (student_id)
 );
 
+-- Notes:
+--  For 'term', could use the regex from COMP3311 23T3 assignment 2 - '[12][01239]T[0123]',
+--      but sqlite doesn't distribute a regular expression implementation by default. I can't be bothered getting it working..
+--  Could also define grade as something like  grade TEXT CHECK (grade IN ("HD", "DN", "CR", "PS", "FL")),
+--      but this would stop me putting a flag in the grade column.
 DROP TABLE IF EXISTS Marks;
 CREATE TABLE Marks (
     student_id INTEGER,
     course_code TEXT,
-    term TEXT, -- Could use the regex from COMP3311 23T3 assignment 2 - '[12][01239]T[0123]' - but I can't figure out how to do it in sqlite3
+    term TEXT,
     mark INTEGER CHECK (mark BETWEEN 0 AND 100),
-    grade TEXT, -- CHECK (grade IN ("HD", "DN", "CR", "PS", "FL")), -- Simplified model
+    grade TEXT,
     PRIMARY KEY (student_id, course_code),
     FOREIGN KEY (student_id) REFERENCES Students(student_id)
 );
